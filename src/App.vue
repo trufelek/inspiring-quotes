dc6afd51d07edb3ef33983dfd123d58bb8ab79e6
Aleksandra Wierzbiak <a.wierzbiak@wearevirtua.com>
2019-03-19 15:09:54 +0100

    inspiring quotes app

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <app-header :quotesCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <new-quote @quoteAdded="newQuote"></new-quote>
+        <quotes :quotes="quotes" @quoteDeleted="deleteQuote"></quotes>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import appHeader from './components/Header'
+    import Quotes from './components/Quotes'
+    import NewQuote from './components/NewQuote'
+
+    export default {
+        data() {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appHeader,
+            NewQuote,
+            Quotes
+        },
+        methods: {
+            newQuote(q) {
+                if(this.quotes.length >= this.maxQuotes) {
+                  return alert('Please remove some quotes!')
+                }
+
+                this.quotes.push(q);
+            },
+            deleteQuote(i) {
+                this.quotes.splice(i, 1);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="quotes-grid row">
+        <app-quote v-for="(quote, i) in quotes" :key="i" @click.native="deleteQuote(i)">{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+    import appQuote from './Quote'
+
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote
+        },
+        methods: {
+            deleteQuote(i) {
+                this.$emit('quoteDeleted', i);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
